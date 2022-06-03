9ed1aa31a31cb523698bd5c369298bb4e1bae4ce
Angelo Ramos <20192258@pucp.edu.pe>
2022-06-02 22:24:30 -0500

    nuevo metodo ladrar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Holaaa");
+
+        Perro perro = new Perro();
+
+        perro.ladrar();
     }
+
 }
--- src/com/company/Perro.java
+++ src/com/company/Perro.java
@@ -12,11 +12,25 @@ public class Perro {
     private String nombre;
     private String apellido;
 
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    private int edad;
+
     public String getApellido() {
         return apellido;
     }
 
     public void setApellido(String apellido) {
         this.apellido = apellido;
+    }
+
+    public void ladrar(){
+        System.out.println("guaguaguu");
     }
 }
