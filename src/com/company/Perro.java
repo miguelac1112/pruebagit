0e293ec27ed36b9ccb1037883984d8ce38c65ed8
Angelo Ramos <20192258@pucp.edu.pe>
2022-06-02 22:17:30 -0500

    Clase perro nueva

---
+++ src/com/company/Perro.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Perro {
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    private String nombre;
+}
