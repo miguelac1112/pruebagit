8ef5a521539f3cf979556c5c4a223e8fc66944b3
Miguel <a20190050@pucp.edu.pe>
2022-06-02 22:21:12 -0500

    modifico perro

--- src/com/company/Perro.java
+++ src/com/company/Perro.java
@@ -10,4 +10,13 @@ public class Perro {
     }
 
     private String nombre;
+    private String apellido;
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 }
